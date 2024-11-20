7f7a6f993be266d384230b15ff9b3a52b4fa3b5b
Sneha-2804 <sagupta28.04@gmail.com>
2024-11-20 16:35:55 +0530

    Contact Section

--- sneha_gupta_task4/lib/constants/nav_items.dart
+++ sneha_gupta_task4/lib/constants/nav_items.dart
@@ -4,7 +4,6 @@ List<String> navTitles = [
   "Home",
   "Skills",
   "Projects",
-  "Blog",
   "Contact",
 ];
 
@@ -12,6 +11,5 @@ List<IconData> navIcons = [
   Icons.home,
   Icons.handyman_outlined,
   Icons.apps,
-  Icons.web,
   Icons.quick_contacts_mail,
 ];
--- sneha_gupta_task4/lib/pages/home_page.dart
+++ sneha_gupta_task4/lib/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:my_portfolio/constants/colors.dart';
 import 'package:my_portfolio/constants/size.dart';
+import 'package:my_portfolio/widgets/contact_section.dart';
 // import 'package:my_portfolio/constants/nav_items.dart';
 import 'package:my_portfolio/widgets/drawer_mobile.dart';
 // import 'package:my_portfolio/styles/style.dart';
@@ -29,68 +30,68 @@ class _HomePageState extends State<HomePage> {
     final screenHeight = screenSize.height;
     return LayoutBuilder(builder: (context, constraints) {
       return Scaffold(
-          key: scaffoldKey,
-          backgroundColor: CustomColor.scaffoldBg,
-          endDrawer:
-              constraints.maxWidth >= kMinDesktopWidth ? null : DrawerMobile(),
-          body: ListView(
-            scrollDirection: Axis.vertical,
-            children: [
-              if (constraints.maxWidth >= kMinDesktopWidth)
-                HeaderDesktop()
-              else
-                HeaderMobile(
-                  onMenuTap: () {
-                    scaffoldKey.currentState?.openEndDrawer();
-                  },
-                  onLogoTap: () {},
-                ),
-              if (constraints.maxWidth >= kMinDesktopWidth)
-                MainDesktop()
-              else
-                MainMobile(),
-              Container(
-                // key: navbarKeys[1],
-                width: screenWidth,
-                padding: const EdgeInsets.fromLTRB(25, 20, 25, 60),
-                color: CustomColor.bgLight1,
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    // title
-                    const Text(
-                      "What I can do",
-                      style: TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
-                        color: CustomColor.whitePrimary,
-                      ),
+        key: scaffoldKey,
+        backgroundColor: CustomColor.scaffoldBg,
+        endDrawer:
+            constraints.maxWidth >= kMinDesktopWidth ? null : DrawerMobile(),
+        body: ListView(
+          scrollDirection: Axis.vertical,
+          children: [
+            if (constraints.maxWidth >= kMinDesktopWidth)
+              HeaderDesktop()
+            else
+              HeaderMobile(
+                onMenuTap: () {
+                  scaffoldKey.currentState?.openEndDrawer();
+                },
+                onLogoTap: () {},
+              ),
+            if (constraints.maxWidth >= kMinDesktopWidth)
+              MainDesktop()
+            else
+              MainMobile(),
+            Container(
+              // key: navbarKeys[1],
+              width: screenWidth,
+              padding: const EdgeInsets.fromLTRB(25, 20, 25, 60),
+              color: CustomColor.bgLight1,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  // title
+                  const Text(
+                    "What I can do",
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: CustomColor.whitePrimary,
                     ),
-                    const SizedBox(height: 50),
+                  ),
+                  const SizedBox(height: 50),
 
-                    if (constraints.maxWidth >= kMedDesktopWidth)
-                      const SkillDesktop()
-                    else
-                      const SkillMobile(),
-                  ],
-                ),
+                  if (constraints.maxWidth >= kMedDesktopWidth)
+                    const SkillDesktop()
+                  else
+                    const SkillMobile(),
+                ],
               ),
-              const SizedBox(height: 30),
-              Container(
-                height: 500,
-                width: double.maxFinite,
-              ),
-              Container(
-                height: 500,
-                width: double.maxFinite,
-                color: Colors.blueGrey,
-              ),
-              Container(
-                height: 500,
-                width: double.maxFinite,
-              ),
-            ],
-          ));
+            ),
+            const SizedBox(height: 30),
+            Container(
+              height: 500,
+              width: double.maxFinite,
+            ),
+            Container(
+              height: 500,
+              width: double.maxFinite,
+              color: Colors.blueGrey,
+            ),
+            SizedBox(height: 20),
+            ContactSection(),
+            SizedBox(height: 20),
+          ],
+        ),
+      );
     });
   }
 }
